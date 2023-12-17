e52972512bed347af5d8201b4c728ea4c8069ef9
CKsisodia <cksisodia3@gmail.com>
2023-12-17 17:21:08 +0530

    email js

---
+++ src/app/emailForm/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { useState } from "react";
+import { TextField, Button, Typography, Box } from "@mui/material";
+import emailjs from "@emailjs/browser";
+import axios from "axios";
+
+const EmailForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log("form submit");
+
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    const data = {
+      service_id: serviceID,
+      template_id: templateID,
+      user_id: publicKey,
+      template_params: {
+        from_name: name,
+        from_email: email,
+        to_name: "chandrakiran",
+        message: message,
+      },
+    };
+    try {
+      const res = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        data
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth: 600,
+          mx: "auto",
+          p: 2,
+          border: "2px solid  #000000",
+          borderRadius: "12px",
+          boxShadow: 1,
+        }}
+      >
+        <Typography variant="h4" align="center" mb={2}>
+          Send Email
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            margin="normal"
+            required
+          />
+          <TextField
+            fullWidth
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            margin="normal"
+            required
+            type="email"
+          />
+          <TextField
+            fullWidth
+            label="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            margin="normal"
+            required
+            multiline
+            rows={4}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            sx={{
+              mt: 2,
+              backgroundColor: "#000",
+              color: "#fff",
+              "&:hover": {
+                backgroundColor: "#111",
+              },
+            }}
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Box>
+  );
+};
+export default EmailForm;
